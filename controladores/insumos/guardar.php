51218df541d72212146ecc185ebb3b4eb01d5373
Nozen <ramirezedelvalle@gmail.com>
2023-05-22 08:46:13 -0600

    edicion estetica

--- controladores/insumos/guardar.php
+++ controladores/insumos/guardar.php
@@ -28,9 +28,35 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Insumos Adquiridos</title>
+    <link rel="shortcut icon" href="images/divisa.png">
 </head>
+<style>
+    body {
+	background-image: url(images/internet.jpeg);
+	background-size: full;
+	background-attachment: fixed;
+    font-family: arial bold;
+    font-size: x-large;
+}
 
+.header {
+  background-image: url(images/ciberdefensa.jpg);
+  background-size: cover;
+  margin: 0;
+  padding: 1px;
+  text-align: center;
+  color: gold;
+  font-family: 'arial';
+  font-size: 60px;
+  transition-duration: 5s;
+}
+
+</style>
 <body>
+<div class="header">
+	<center><h1> COMANDO DE INFORMATICA Y TECNOLOGIA</h1></center>
+	<center><p>Oficina de Logistica</p></center>
+</div> 
     <div class="container">
         <div class="row">
             <div class="col-lg-6">
